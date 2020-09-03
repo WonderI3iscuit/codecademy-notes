a20000e0069b128566970eb4c166a040605f8775
Ben McElhinny <benmcelhinny2003@gmail.com>
2020-09-03 09:19:38 -0400

    Add project files.

---
+++ codecademy notes/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System;
+
+namespace GettingInput
+{
+    class Program
+    {
+        static void Main()
+        {
+            //this jargen asks the user how old they are
+            Console.WriteLine("How old are you?");
+            string input = Console.ReadLine();
+            Console.WriteLine($"You are {input} years old!");
+        }
+    }
+}
